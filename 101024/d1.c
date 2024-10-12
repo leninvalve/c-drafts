89bd872205f5ead9ba76095fcd1d6ffa911cbf87
Lenin Valverde <leninvalve@gmail.com>
2024-10-11 23:08:36 -0500

    added comments for better understanding

--- 101024/d1.c
+++ 101024/d1.c
@@ -4,12 +4,23 @@ int read_line(char line[]);
 
 int main()
 {
+	// Declares a character array to hold up to 4 elements.
 	char line[4];
 	int i;
 
+	// Loop runs till read_line() returns a 0.
+	// This is because C treats the integer 0 as false
+	// and any other integer as true.
+	// The test part of the while would also be:
+	// while (read_line(line) > 0)
+	// If read_line(line) returns 0, then 0 > 0 evaluates to false
+	// and you leave the loop.
+	// Which one is better? For readability I'd prefer the one above.
+	// My current knowledge ignores whether or not using one or the other
+	// modifies the performance of the program.
 	while (read_line(line)) {
 		printf(">> Array repr: ");
-		putchar('[');
+		putchar('[ ');
 		for (i = 0; i < 4; i++)
 			printf("(%c) ", line[i]);
 		putchar(']');
