45163a45c96fb4c3f28d3a24f7a51fa7016fac08
Lenin Valverde <leninvalve@gmail.com>
2024-10-12 00:07:46 -0500

    just some comments

--- book-exercises/ch-1/1-1.c
+++ book-exercises/ch-1/1-1.c
@@ -1,8 +1,21 @@
 // include information about standard library
+// without it, 'printf' function could not
+// be used.
 #include <stdio.h>
 
+// 
 int main(void)
 {
+	// Call the stdio library function 'printf'.
+	// Parentheses after the function name contain
+	// a list of values, data to be communicated
+	// to the printf function. Such values are named
+	// 'arguments'.
+	// So, arguments is a method for communicating values
+ 	// functions.
+
+	// It is also a statement, since it ends with a 
+	// semicolon. Do all statements end with a semicolon?
 	printf("hello, world\n");
 	return 0;
 }
